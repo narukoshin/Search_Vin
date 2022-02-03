1ef007285b8269bc29c93a5c8b1eb8056f303665
Naru Koshin <naru.koshin@outlook.jp>
2022-02-03 20:13:04 +0200

    finished the last part for full functionality

--- main.class.php
+++ main.class.php
@@ -7,6 +7,19 @@
          */
         private string $plate_number;
         /**
+         * @var object
+         */
+        private object $request;
+        /**
+         * 
+         */
+        public function __construct(){
+            $this->request = (object)[
+                "get_code" => "https://uzzinivin.lv/?rn=%s",
+                "get_vin"  => "https://uzzinivin.lv/v/%s"
+            ];
+        }
+        /**
          * User Agent for the requests
          * 
          * @var string
@@ -16,14 +29,61 @@
          * Setting the car's plate number to get the VIN number
          * 
          * @param string
-         * @return void
+         * @return $this
          */
         public function set_plate(string $plate) {
-            $this->plate_number = $plate;
+            $this->plate_number = str_replace(" ", "", $plate);
+            return $this;
+        }
+        /** 
+         * Find the VIN number by car plate
+         * 
+         * @return void
+         * 
+        */
+        public function find(){
+            $code = $this->_get_code();
+            $vin  = $this->_get_vin($code);
+            print_r( $vin );
+        }
+        /**
+         * @param string $method
+         * @param string $uri
+         * 
+         * @return string
+         */
+        private function NewRequest(string $method, string $uri) {
+            return file_get_contents($uri, false, stream_context_create([
+                "http" => [
+                    "method" => $method,
+                    "header" => join("\r\n", [
+                        "User-Agent: " . $this->user_agent
+                    ])
+                ]
+            ]));
+        }
+        /** 
+         * Getting the base64 encoded code that will help to get VIN number in the next request
+         * 
+         * @return string
+        */
+        private function _get_code(){
+            $string = $this->NewRequest("GET", sprintf($this->request->get_code, $this->plate_number));
+            if (!$string) return;
+            preg_match("/window\.location\.href\=\"https\:\/\/[a-zA-Z0-9]+\.[a-zA-Z]+\/v\/(.*)\"\;/", $string, $match);
+            return end($match);
+        }
+        /**
+         * Getting the VIN
+         * 
+         * @param string $code
+         * @return string
+         */
+        private function _get_vin(string $code){
+            $string = $this->NewRequest("GET", sprintf($this->request->get_vin, $code));
+            if (!$string) return;
+            preg_match("/https\:\/\/www\.autodna\.lv\/vin\/(.*)\"/", $string, $match);
+            return current(explode("\"", $match[1]));
         }
     }
-    // declaring the class
-    $vin = new Vin_Search;
-
-    // setting setting the car plate number
-    $vin->set_plate("NA25");
+    echo (new Vin_Search)->set_plate("JL 152")->find();
